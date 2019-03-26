3408b02cc73f6d4dc8dffa81d4afb8fe5d1a6c67
Jack Franklin <jack@jackfranklin.net>
2019-03-26 16:05:34 +0000

    issues with mocks

---
+++ issues-with-mocks/index.test.js
@@ -0,0 +1,39 @@
+jest.mock('./fetch-employees')
+
+const fetchEmployees = require('./fetch-employees')
+const findEmployees = require('./index')
+
+describe('filtering employees', () => {
+  it('can filter for employees based off tenure', () => {
+    fetchEmployees.mockImplementation(() => {
+      return Promise.resolve({
+        employees: [
+          { name: 'Alice', department: 'Engineering', tenure: 3 },
+          { name: 'Bob', department: 'HR', tenure: 2 },
+        ],
+      })
+    })
+
+    return findEmployees({ tenure: 2 }).then(employees => {
+      expect(employees).toEqual([
+        {
+          name: 'Bob',
+          department: 'HR',
+          tenure: 2,
+        },
+      ])
+    })
+  })
+
+  it('can filter based on department', () => {
+    return findEmployees({ department: 'Engineering' }).then(employees => {
+      expect(employees).toEqual([
+        {
+          name: 'Alice',
+          department: 'Engineering',
+          tenure: 5,
+        },
+      ])
+    })
+  })
+})
---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // remove the comment when we get to this part of the exercise :)
+  // restoreMocks: true,
+}
