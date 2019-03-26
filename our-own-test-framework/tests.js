b2ce05e0a6eb90ea5d436555647cded387db7a56
Jack Franklin <jack@jackfranklin.net>
2019-03-26 10:40:38 +0000

    prettierify

--- our-own-test-framework/tests.js
+++ our-own-test-framework/tests.js
@@ -2,7 +2,6 @@ const assert = require('assert')
 const findWordsOfLength = require('./index')
 const { test } = require('./framework')
 
-
 test('It pulls out the words of the right length', () => {
   const result = findWordsOfLength('The person went walking', 3)
   assert.deepStrictEqual(result, ['The'])
--- using-jest/index.test.js
+++ using-jest/index.test.js
@@ -1,8 +1,8 @@
 const findWordsOfLength = require('./index')
 
 // TODO: rewrite this test using Jest
-  // const result = findWordsOfLength('The person went walking', 3)
-  // assert.deepStrictEqual(result, ['The'])
+// const result = findWordsOfLength('The person went walking', 3)
+// assert.deepStrictEqual(result, ['The'])
 
 test('It finds words only of the given length, not ones that are shorter', () => {
   const result = findWordsOfLength('The person went for a walk', 3)
