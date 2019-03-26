2966107ce9134452ec350f5b595f2e8d39566909
Jack Franklin <jack@jackfranklin.net>
2019-03-26 10:39:28 +0000

    prettier stuff

---
+++ finding-employees-tdd/index.js
@@ -0,0 +1,14 @@
+const employees = [
+  { name: 'Alice', department: 'Engineering', tenure: 5 },
+  { name: 'Bob', department: 'HR', tenure: 2 },
+  { name: 'Charlie', department: 'Finance', tenure: 3 },
+  { name: 'Danielle', department: 'Engineering', tenure: 1 },
+  { name: 'Edward', department: 'Engineering', tenure: 7 },
+  { name: 'Grace', department: 'Design', tenure: 4 },
+]
+
+const findEmployees = filters => {
+  // ???
+}
+
+module.exports = findEmployees
---
+++ finding-employees-tdd/index.test.js
@@ -0,0 +1,3 @@
+const findEmployees = require('./index')
+
+describe('filtering employees', () => {})
