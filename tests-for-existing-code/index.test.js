d9ae85a38ae57f4b8d583450632568963eb80407
Jack Franklin <jack@jackfranklin.net>
2019-03-27 13:38:30 +0100

    set up tests for this

--- tests-for-existing-code/index.test.js
+++ tests-for-existing-code/index.test.js
@@ -1,7 +1,13 @@
 const EventHub = require('./index')
 
 describe('EventHub', () => {
-  it('works', () => {
-    // TODO: write some tests
+  it('calls the listener when the event happens', () => {
+    let called = false
+
+    // TODO: test the event hub here and fix the assertion
+
+    expect(called).toEqual(true)
   })
+
+  // TODO: write any more tests that you think this module should have
 })
